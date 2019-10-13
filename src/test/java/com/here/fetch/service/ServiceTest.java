d02e0592489d2ba259fe99d2e359dffbb45fbc5e
Dima Shchepin <dimashchepin@gmail.com>
2019-10-13 01:49:43 -0500

    add test for service

--- src/test/java/com/here/fetch/service/ServiceTest.java
+++ src/test/java/com/here/fetch/service/ServiceTest.java
@@ -1,2 +1,27 @@
+package com.here.fetch.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
-**/
+ * Unit tests for sampleService class.
+ *
+ * @author Dmitriy Shternberg <dmitriy.shternberg@here.com>
+ */
+public class ServiceTest {
+
+    private SampleServiceForWeb instance = new SampleServiceForWeb();
+
+    @Test
+    public void testLettersOutputWrong() throws Exception {
+        Boolean result = instance.isWordRight("abba");
+        Assert.assertEquals(result, false);
+    }
+
+    @Test
+    public void testLettersOutputRight() throws Exception {
+        Boolean result = instance.isWordRight("ill");
+        Assert.assertEquals(result, true);
+    }
+
+}
