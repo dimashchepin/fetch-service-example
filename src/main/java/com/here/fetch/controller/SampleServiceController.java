2fafc74fc20d6268fb5fbcb8ee7d1d1e2b4438a4
Dima Shchepin <dimashchepin@gmail.com>
2019-10-13 01:47:35 -0500

    add controller for service

--- src/main/java/com/here/fetch/controller/SampleServiceController.java
+++ src/main/java/com/here/fetch/controller/SampleServiceController.java
@@ -1,2 +1,31 @@
-/** 
-**/
+package com.here.fetch.controller;
+
+import com.here.fetch.service.SampleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Service controller.
+ *
+ * @author Dmitriy Shternberg <dimashchepin@gmail.com>
+ */
+@RestController
+public class SampleServiceController {
+
+    @Autowired
+    SampleService productService;
+
+    /**
+     * Service to analyze word letter frequency.
+     *
+     * @param word word to be analyzed
+     * @return get response - "true" if word is "right" else "false"
+     */
+    @RequestMapping(value = "/letters/{word}")
+    public ResponseEntity<Object> getRightWord(@PathVariable("word") String word) {
+        return new ResponseEntity<>(productService.isWordRight(word), HttpStatus.OK);
+    }
+
+}
