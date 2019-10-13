98469dc72bbe02e0d7d6f8776d25bae05b89df1c
Dima Shchepin <dimashchepin@gmail.com>
2019-10-13 01:51:18 -0500

    add test for controller - integration

--- src/test/java/com/here/fetch/controller/SampleServiceControllerTest.java
+++ src/test/java/com/here/fetch/controller/SampleServiceControllerTest.java
@@ -1,2 +1,45 @@
-/** 
-**/
+package com.here.fetch.controller;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+/**
+ * Integration tests for Sample service controller.
+ *
+ * @author Dmitriy Shternberg <dimashchepin@gmail.com>
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SampleServiceControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testIsWordRight() throws Exception {
+        String inputWord = "sssell";
+        mvc.perform(MockMvcRequestBuilders.get("/letters/" + inputWord).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("true")));
+    }
+
+    @Test
+    public void testIsWordWrong() throws Exception {
+        String inputWord = "abba";
+        mvc.perform(MockMvcRequestBuilders.get("/letters/" + inputWord).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("false")));
+    }
+}
