688e17cae6a2e5aa97b9573c54bafc34824597c0
Dima Shchepin <dimashchepin@gmail.com>
2019-10-13 11:05:11 -0500

    update service test

--- src/test/java/com/here/fetch/service/ServiceTest.java
+++ src/test/java/com/here/fetch/service/ServiceTest.java
@@ -13,14 +13,32 @@ public class ServiceTest {
     private SampleServiceForWeb instance = new SampleServiceForWeb();
 
     @Test
-    public void testLettersOutputWrong() throws Exception {
-        Boolean result = instance.isWordRight("abba");
+    public void testIfNotPyramidWord() throws Exception {
+        Boolean result = instance.isWordAPyramid("abba");
         Assert.assertEquals(result, false);
     }
 
     @Test
-    public void testLettersOutputRight() throws Exception {
-        Boolean result = instance.isWordRight("ill");
+    public void testIfNotPyramidWordWhenEmpty() throws Exception {
+        Boolean result = instance.isWordAPyramid("");
+        Assert.assertEquals(result, false);
+    }
+
+    @Test
+    public void testIfNotPyramidWordSecond() throws Exception {
+        Boolean result = instance.isWordAPyramid("bandana");
+        Assert.assertEquals(result, false);
+    }
+
+    @Test
+    public void testIfPyramidWordSmall() throws Exception {
+        Boolean result = instance.isWordAPyramid("ill");
+        Assert.assertEquals(result, true);
+    }
+
+    @Test
+    public void testIfPyramidWordMiddle() throws Exception {
+        Boolean result = instance.isWordAPyramid("banana");
         Assert.assertEquals(result, true);
     }
 
