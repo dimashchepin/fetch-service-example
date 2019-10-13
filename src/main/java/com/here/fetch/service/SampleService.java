b1bacf6e8bcf53b380996b4aceb14ad5c49e3866
Dima Shchepin <dimashchepin@gmail.com>
2019-10-13 01:45:23 -0500

    added service interface and implementation

--- src/main/java/com/here/fetch/service/SampleService.java
+++ src/main/java/com/here/fetch/service/SampleService.java
@@ -1,2 +1,18 @@
-/** 
-**/
+package com.here.fetch.service;
+
+
+/**
+ * Interface for Service methods.
+ *
+ * @author Dmitriy Shternberg <dimashchepin@gmail.com>
+ */
+public interface SampleService {
+
+    /**
+     * Determines if word is "right" (has letter frequency numbers, when sorted, in sequence.
+     *
+     * @param word word to be analyzed
+     * @return true if word is "right" else false
+     */
+    boolean isWordRight(String word);
+}
---
+++ src/main/java/com/here/fetch/service/SampleServiceForWeb.java
@@ -0,0 +1,52 @@
+package com.here.fetch.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+/**
+ * Implementation of Sample service interface.
+ *
+ * @author Dmitriy Shternberg <dimashchepin@gmail.com>
+ */
+@Service
+public class SampleServiceForWeb implements SampleService {
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isWordRight(String word) {
+        String[] rt = word.split("");
+        Map<String, Integer> freqs = generateWordFrequencies(rt);
+        ArrayList<Integer> freqLst = new ArrayList<>(freqs.values());
+        Collections.sort(freqLst);
+        boolean wordRight = true;
+        for(int i = 0;i < freqLst.size();i++){
+            if(i > 0 && freqLst.get(i) - freqLst.get(i-1) != 1){
+                wordRight = false;
+            }
+        }
+
+        return wordRight;
+    }
+
+    /**
+     * Generate letter frequencies map.
+     *
+     * @param rt array of letters in the word
+     * @return frequencies
+     */
+    private Map<String, Integer> generateWordFrequencies(String[] rt) {
+        Map<String, Integer> freqs = new HashMap<>();
+        for(String r : rt) {
+            if (freqs.containsKey(r)){
+                freqs.put(r, 1 + freqs.get(r));
+            } else {
+                freqs.put(r, 1);
+            }
+        }
+        return freqs;
+    }
+}
